fix: sparsity loss computation

src/jepa/autoencoder.py
@@ -153,7 +153,7 @@ class AutoencoderCriterion(nn.Module):
         x = batch["x"]
         x_hat, z = output["x_hat"], output["z"]
         re = nn.functional.mse_loss(x_hat, x)
-        latent_l1norm = torch.norm(z, p=1).mean()
+        latent_l1norm = torch.abs(z).mean()
         loss = re + self.sparsity_weight * latent_l1norm
         return {"loss": loss, "re": re, "latent_l1norm": latent_l1norm}
 

src/jepa/jepa.py
@@ -111,7 +111,7 @@ class JepaCriterion(nn.Module):
         encoder_output = output["encoder_output"]
         predictor_output = output["predictor_output"]
         re = self.re(predictor_output, ema_output)
-        latent_l1norm = torch.norm(encoder_output, p=1).mean()
+        latent_l1norm = torch.abs(encoder_output).mean()
         loss = re + self.sparsity_weight * latent_l1norm
         return {"loss": loss, "re": re, "latent_l1norm": latent_l1norm}
     