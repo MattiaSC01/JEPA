3ad48552419cd734fab54a322680f415031b64e2
MattiaSC01 <mattia.scardecchia@studbocconi.it>
2024-05-24 12:26:40 +0200

    fix: type

--- src/jepa/trainer/trainer.py
+++ src/jepa/trainer/trainer.py
@@ -253,7 +253,7 @@ class Trainer:
         if self.log_to_wandb:
             for key, value in avg_losses.items():
                 self.logger.log_metric(value, f"val/{key}", self.step)
-        return avg_losses["loss"].item()
+        return avg_losses["loss"]
 
     def make_checkpoint(self):
         # TODO: test changes to this method
